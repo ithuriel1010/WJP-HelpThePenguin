cc76d48d736bfded202206a4ba79203f1329b2e0
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2019-12-02 18:26:32 +0100

    some change

--- src/com/company/sleepClick.java
+++ src/com/company/sleepClick.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 
 
 public class sleepClick implements ActionListener {
