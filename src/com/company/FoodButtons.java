24b09ae7c4cd84da9a5032204be0bb4ebb39022c
Aleksandra Wędołowska <ithuriel1010@gmail.com>
2019-12-07 16:01:32 +0100

    Added a class for all food parameters and an enum class to establish the food type

--- src/com/company/FoodButtons.java
+++ src/com/company/FoodButtons.java
@@ -5,10 +5,18 @@ import javax.swing.*;
 public class FoodButtons {
 
     Window w;
-    FoodButtons(JButton button, int calories, int happyPoints, String file, int x, int y)
-    {
-        w = new Window();
-        w.ButtonImage(button, file,x,y,true);
 
-    }
+    FoodButtons(int calories, int happyPoints, int healthPoints, FoodType type)
+    { }
+
+    public static FoodButtons chicken = new FoodButtons(100,100, 100,FoodType.goodfood);
+    public static FoodButtons pizza = new FoodButtons(100,100,50,FoodType.fastfood);
+    public static FoodButtons sandwich = new FoodButtons(100,100, 100, FoodType.goodfood);
+    public static FoodButtons pancake = new FoodButtons(100,100, 75, FoodType.sweets);
+    public static FoodButtons chocolate = new FoodButtons(100,100, 10, FoodType.sweets);
+    public static FoodButtons fish = new FoodButtons(100,100, 100, FoodType.sweets);
+    public static FoodButtons dinner = new FoodButtons(100,100,150, FoodType.goodfood);
+    public static FoodButtons nuggets = new FoodButtons(100,100,25, FoodType.fastfood);
+    public static FoodButtons salad = new FoodButtons(100,100, 150, FoodType.goodfood);
+
 }
